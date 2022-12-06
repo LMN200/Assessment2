52d4cb59841c54ef462cbeb8d14a3806da445ea7
LMN200 <lmnelson2580@gmail.com>
2022-12-06 00:50:03 +0000

    LN

--- Black_Death.py
+++ Black_Death.py
@@ -11,7 +11,7 @@ import csv
 import matplotlib
 
 
-# Load in dataset for population density and rat deaths
+# Load in dataset for population density and rats caught
 with open('population.txt', newline='') as p:
     dataset = csv.reader(p, quoting=csv.QUOTE_NONNUMERIC)
     population = [line for line in dataset]
@@ -21,8 +21,14 @@ with open('rats.txt', newline='') as r:
     dataset = csv.reader(r, quoting=csv.QUOTE_NONNUMERIC)
     rats = [line for line in dataset]
  
-# Test both datasets has been loaded in
+# Test both txt files has been loaded in as raster maps
 matplotlib.pyplot.imshow(population)
 matplotlib.pyplot.imshow(rats)
 
-d = (0.8*r)(1.3*p)
+
+
+
+
+
+# d = (0.8*r)(1.3*p)
+# need to create a new list and run the pairs of data from the rastor map through the equation. Then write to a CSV file.
