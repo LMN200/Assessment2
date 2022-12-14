e3a19f5ff0a6fabcabd2f1e3f9d120e4809852b3
LMN200 <lmnelson2580@gmail.com>
2022-12-14 19:13:23 +0000

    LN

--- Black_Death.py
+++ Black_Death.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 22 15:22:40 2022
+Created on Wed Dec 14 16:56:18 2022
 
 @author: lucynelson
 """
 
-'''
-STEP 1
-'''
 # Import packages
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import tkinter
+from tkinter import simpledialog
 import matplotlib.backends.backend_tkagg as tkagg
 import matplotlib; matplotlib.use("TkAgg")
 import matplotlib.animation
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 population = []
 rats = []
@@ -34,13 +34,12 @@ with open('rats.txt', newline='') as r:
 r.close()
 
 # Test population density has loaded in and display
-print(population[0][:10])
+# print(population[0][:10])
 plt.imshow(population)
 plt.show()
 
-
 # Test rats caught data has loaded in and display
-print(rats[0][:10])
+# print(rats[0][:10])
 plt.imshow(rats)
 plt.show()
 
@@ -48,20 +47,7 @@ plt.show()
 STEP 2
 '''
 
-# deaths = [[] for i in range(len(population))]
-
-# for i in range(len(population)):
-#     line_deaths = []
-#     for j in range(len(population[i])):
-#         line_deaths.append((0.8*rats[i][j])*(1.3*population[i][j]))
-#     deaths[i] = line_deaths
-
-# print(len(deaths))
-
-# plt.imshow(deaths)
-
-
-# use numpy to create a daa array with the same length parameters as population
+# use numpy to create a data array with the same length parameters as population
 deaths = np.zeros((len(population), len(population[0])))
 print(deaths)
 
@@ -71,60 +57,80 @@ for i in range(len(population)):
     for j in range(len(population[i])):
         deaths[i][j] = ((0.8*rats[i][j])*(1.3*population[i][j]))
 
-# plot deaths
-plt.imshow(deaths)
+# # plot deaths
+# plt.imshow(deaths)
+# plt.show()
+
+
+# define calculation for deaths using weighting. set default for the weights as 1. 
+def calc_deaths(population=None, rats=None, pop_weight=1, rats_weight=1):
+    # don't forget scope of population is within this function. This means population doesn't get changed on the outside
+    # All this does is go through a list of lists and multiply every value by the weighting
+    #
+    population = [[entry * pop_weight for entry in line] for line in population]
+    rats = [[entry * rats_weight for entry in line] for line in rats]
+
+    deaths = np.zeros((len(population), len(population[0])))
+    print(deaths)
+
+    # calculate deaths for every line in the list of population densities using the equation
+    # we don't need to multiply by the weighting here as its already been done for us earlier
+    for i in range(len(population)):
+        for j in range(len(population[i])):
+            deaths[i][j] = ((0.8 * rats[i][j]) * (1.3 * population[i][j]))
+
+    # This effectivly retuns a tuple of three things
+    return deaths, population, rats
+
 
 '''
 STEP 3
 '''
-# Display all 3 maps
-f, [ax1, ax2, ax3] = plt.subplots(1,3, figsize= (12 , 3))
-pp = ax1.imshow(population, cmap = 'OrRd')
-rr = ax2.imshow(rats, cmap = 'Blues')
-dd = ax3.imshow(deaths, cmap = 'Greens')
-# add titles
-ax1.set_title('Population Density')
-ax2.set_title('Rats Caught per Week')
-ax3.set_title('Deaths per Week')
-# add colorbars
-f.colorbar(pp, ax=ax1)
-f.colorbar(rr, ax=ax2)
-f.colorbar(dd, ax=ax3)
 
+def set_up_canvas(deaths=None, rats=None, population=None):
+    # Display all 3 maps
+    f, [ax1, ax2, ax3] = plt.subplots(1, 3, figsize=(12, 3))
+    pp = ax1.imshow(population, cmap='OrRd')
+    rr = ax2.imshow(rats, cmap='Blues')
+    dd = ax3.imshow(deaths, cmap='Greens')
+    # add titles
+    ax1.set_title('Population Density')
+    ax2.set_title('Rats Caught per Week')
+    ax3.set_title('Deaths per Week')
+    # add colorbars
+    f.colorbar(pp, ax=ax1)
+    f.colorbar(rr, ax=ax2)
+    f.colorbar(dd, ax=ax3)
+
+    return f
 
 '''
 STEP 4
 '''
-# # # save numpy array to a new txt file
-# np.savetxt('absolute_deaths.txt', deaths, delimiter = ',', newline = '', fmt = '%i')
+# Write to a txt file
+# Put deaths into a Pandas dataframe, rounding numbers to 3 decimals places
+df_deaths = pd.DataFrame(deaths)
+df_deaths = df_deaths.astype(float).round(3)
 
-# # check it has worked and produces same map as deaths list
-# # with open('absolute_deaths.txt', newline='') as ad:
-# #     dataset3 = csv.reader(ad, quoting=csv.QUOTE_NONNUMERIC)
-# #     absolute_deaths = [line for line in dataset3]
-# # ad.close()
+# Write the deaths dataframe to a txt file
+df_deaths.to_csv('absolute_deaths.txt')
 
-# absolute_deaths = np.loadtxt('absolute_deaths.txt', dtype = int, delimiter = ',')
-
-# plt.imshow(absolute_deaths)
-
-
-# with open('absolute_deaths2.txt', 'w') as ab:
-#     for item in deaths:
-#         # write each item on a new line
-#         ab.write("%s\n" % item)
-
-# with open('absolute_deaths2.txt', newline='') as ab2:
-#     dataset3 = csv.reader(ab2, quoting=csv.QUOTE_NONNUMERIC)
-#     absolute_deaths = [line for line in dataset3]
-# ab2.close()
+# # Test it has worked
+# # Read the saved absolute_deaths txt file back in
+# df_new_deaths = pd.read_csv ('absolute_deaths.txt')
+# # drop the exra columns
+# cols_to_drop = ['Unnamed: 0']
+# df_new_deaths.drop(columns=cols_to_drop, inplace=True)
+# # Plot on graph to check it matches the original deaths graph. 
+# plt.imshow(df_new_deaths)
 
 '''
 STEP 5
 '''
-# total deaths per week. 
+# total overall deaths per week as a count
 
-
+overall_deaths = (deaths.sum()) / 10**4 
+print(f'Overall Deaths per Week = {overall_deaths}')
 
 '''
 STEP 6
@@ -132,29 +138,95 @@ STEP 6
 # Allows the user to change the parameter weights for the equation (for example with a scrollbar).
 # Create GUI
 
-def run():
-    pass 
+def run(root):
+
+    if not root:
+        pass
+    else:
+        root.destroy()
+
+    popup = tkinter.Tk()
+
+    popup.withdraw()
+    # popup.minsize(1800, 1800)
+    # popup.focus_set()
+    # the input dialog
+    pop_weight = simpledialog.askstring(title="Test",
+                                      prompt="Enter weight to population density:")
+
+    if pop_weight.isnumeric():
+        pop_weight = float(pop_weight)
+    else:
+        print('input is not numeric weighting will be set to 1')
+        pop_weight = 1
 
 
-root = tkinter.Tk() 
-root.wm_title("Model")
+    # check it out
+    print("Hello", pop_weight)
 
-canvas = tkagg.FigureCanvasTkAgg(f, master=root)
-canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+    popup.destroy()
 
-menu = tkinter.Menu(root)  # set up menu
-root.config(menu=menu)
+    popup = tkinter.Tk()
 
-model_menu = tkinter.Menu(menu)
-menu.add_cascade(label="Model", menu=model_menu)
+    popup.withdraw()
+    # the input dialog
+    rats_weight = simpledialog.askstring(title="Test",
+                                      prompt="Enter weight to rats_caught:")
 
-model_menu.add_command(label="Run model", command=run())
+    if rats_weight.isnumeric():
+        rats_weight = float(pop_weight)
+    else:
+        print('input is not numeric weighting will be set to 1')
+        rats_weight = 1
 
-w = tkinter.Canvas(root, width=200, height=200)
-w.pack()
-w.create_rectangle(0, 0, 200, 200, fill="blue")
 
-print('we got this far')
-canvas.draw()
+    # check it out
+    print("Hello", rats_weight)
 
-tkinter.mainloop()
+    popup.destroy()
+
+
+
+    # pop_weight = float(input("Enter weight to population density:"))
+    # rats_weight = float(input("Enter weight to rats_caught:"))
+    deaths, new_pop, new_rats = calc_deaths(population=population, rats=rats, pop_weight=pop_weight, rats_weight=rats_weight)
+
+    root = tkinter.Tk()
+    root.wm_title("Model")
+    f = set_up_canvas(deaths=deaths, rats=new_rats, population=new_pop)
+
+    canvas = tkagg.FigureCanvasTkAgg(f, master=root)
+    canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    # button that displays the plot
+    plot_button = tkinter.Button(master=root,
+                                  command=lambda: run(root),
+                                  height=2,
+                                  width=30,
+                                  bg="red", fg="yellow",
+                                  text="Change weightings")
+
+    # place the button
+    # in main window
+    plot_button.pack()
+
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas, root)
+    toolbar.update()
+
+    menubar = tkinter.Menu(root)  # set up menu
+    root.config(menu=menubar)
+
+    model_menu = tkinter.Menu(menubar)
+
+    menubar.add_separator()
+
+    menubar.add_cascade(label="Model", menu=model_menu)
+    menubar.add_command(label="Exit", command=exit)
+
+    print('we got this far')
+    # canvas.draw()
+
+    tkinter.mainloop()
+
+run(False)
